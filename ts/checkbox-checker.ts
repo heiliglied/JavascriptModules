a7da2a49e20e55ad313cc388dba88dd78b59e174
hungrysorrow <heiliglied@gmail.com>
2022-07-22 15:16:53 +0900

    Update checkbox-checker.ts

--- ts/checkbox-checker.ts
+++ ts/checkbox-checker.ts
@@ -1,3 +1,13 @@
+/*
+# 체크박스 다중선택을 위한 스크립트.  
+typescript 연습용으로 ts로 만들어 봄.  
+
+### 사용법  
+ - let checker = new heiliglied.CheckboxChecker();
+ - checker.changeStatus(전체선택 selector, 다중선택 할 체크박스의 selector값);
+ - addEventListener를 이용하기 때문에 항목이 수십, 수백건이 되면 약간의 부하가 생김.
+*/
+
 namespace heiliglied {
     export class CheckboxChecker {
         totalChecker: string;
