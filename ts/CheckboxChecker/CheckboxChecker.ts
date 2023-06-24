062edd3ef381d7a02b89a8bf35c217c0cdc4a184
hungrysorrow <heiliglied@gmail.com>
2023-06-24 19:59:30 +0900

    Create CheckboxChecker.ts

---
+++ ts/CheckboxChecker/CheckboxChecker.ts
@@ -0,0 +1,45 @@
+namespace heiliglied {
+    export class CheckboxChecker {
+        totalChecker: string;
+        checkboxName: string;
+        constructor() {}
+
+        changeStatus(totalChecker: string, checkboxes: string) {
+            this.totalChecker = totalChecker;
+            this.checkboxes = checkboxes;
+            let totalBox = <HTMLInputElement>document.querySelector(this.totalChecker);
+            let checkbox = document.querySelectorAll(this.checkboxes);
+
+            totalBox.addEventListener('change', function(){
+	            let checked;
+	            if(this.checked == true) {
+		            checked = true;
+	            } else {
+		            checked = false;
+	            }
+
+	            for(let i = 0; i < checkbox.length; i++) {
+                    let checklist = <HTMLInputElement>checkbox[i];
+                    checklist.checked = checked;
+                }
+            });
+
+            Array.from(checkbox).forEach((el, index)=>{
+                el.addEventListener('change', ()=>{
+                    let checkedCount: number = 0;
+                    Array.from(checkbox).forEach((el, index) => {
+                        let checkboxes = <HTMLInputElement>el;
+                        if(checkboxes.checked == true) {
+                            checkedCount++;
+                        }
+                    });
+                    if(checkedCount >= checkbox.length) {
+                        totalBox.checked = true;
+                    } else {
+                        totalBox.checked = false;
+                    }
+                });
+            });
+        }
+    }
+}
