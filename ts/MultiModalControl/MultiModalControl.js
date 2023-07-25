c4629c14df6a590dce15c348d92d32a6af3c9601
hungrysorrow <heiliglied@gmail.com>
2023-07-25 15:25:07 +0900

    Update MultiModalControl.js
    일반 텍스트 모달 함수 추가.

--- ts/MultiModalControl/MultiModalControl.js
+++ ts/MultiModalControl/MultiModalControl.js
@@ -115,6 +115,78 @@ var MultiModalControl = /** @class */ (function () {
         (_a = document.getElementById(modalId)) === null || _a === void 0 ? void 0 : _a.remove();
         (_b = document.getElementById(vailId)) === null || _b === void 0 ? void 0 : _b.remove();
     };
+    MultiModalControl.prototype.setModalReference = function (depth, vail, draggable, contents) {
+        this.draggable = draggable;
+        var vailId = 'multi-vail-' + depth;
+        if (vail == true) {
+            var vailElement = document.createElement('div');
+            vailElement.setAttribute('id', vailId);
+            vailElement.setAttribute('style', 'position:fixed; top:0; left:0; bottom: 0; width:100%; height:100%; background:#000; z-index:' + (this.zIndex + depth) + '; opacity:0.3;');
+            document.body.insertAdjacentElement('beforeend', vailElement);
+        }
+        var modalElement = document.createElement('div');
+        var modalId = 'multi-modal-' + depth;
+        var modalWidth = '480px';
+        var modalHeight = 'auto';
+        modalElement.setAttribute('id', modalId);
+        modalElement.setAttribute('style', 'position: fixed; width: ' + modalWidth + '; height:' + modalHeight + '; top: 20; left: 50%; transform: translateX(-50%); background:#FFF; z-index:' + (this.zIndex + depth + 1) + ';');
+        var mouseClick = this.mouseClick;
+        var posX = this.posX;
+        var posY = this.posY;
+        if (this.draggable == true) {
+            var dragElement = document.createElement('div');
+            dragElement.style.width = modalWidth;
+            dragElement.style.height = '20px';
+            dragElement.style.backgroundColor = "#abcdef";
+            modalHeight = String(Number(modalHeight.replace('px', '')) + 20) + 'px';
+            modalElement.insertAdjacentElement('beforeend', dragElement);
+            dragElement.addEventListener('mousedown', function (event) {
+                mouseClick = true;
+                posX = event.clientX;
+                posY = event.clientY;
+            });
+            dragElement.addEventListener('mousemove', function (event) {
+                if (mouseClick == true) {
+                    var parentElement = this.parentElement;
+                    var now_posX = posX - event.clientX;
+                    var now_posY = posY - event.clientY;
+                    posX = event.clientX;
+                    posY = event.clientY;
+                    parentElement.style.left = (parentElement.offsetLeft - now_posX) + "px";
+                    parentElement.style.top = (parentElement.offsetTop - now_posY) + "px";
+                }
+            });
+            dragElement.addEventListener('mouseup', function (event) {
+                mouseClick = false;
+            });
+            document.addEventListener('mouseup', function (event) {
+                mouseClick = false;
+            });
+        }
+        if (this.bodyFix == true) {
+            document.querySelector('body').style.overflow = 'hidden';
+        }
+        var contentElemnt = document.createElement('div');
+        contentElemnt.setAttribute('style', 'width: calc(100% - 10px); padding: 5px; background-color: white;');
+        contentElemnt.innerText = contents;
+        var footerElement = document.createElement('div');
+        footerElement.setAttribute('style', 'width: calc(100% - 10px); border-top: 2px #000000 solid; padding: 5px; background-color: white; text-align: right');
+        var closeButton = document.createElement('button');
+        closeButton.setAttribute('type', 'button');
+        closeButton.setAttribute('id', 'multi-button-' + depth);
+        closeButton.innerText = '닫기';
+        footerElement.insertAdjacentElement('beforeend', closeButton);
+        closeButton.addEventListener('click', function (event) {
+            var button_depth = this.getAttribute('id');
+            var depth_number = button_depth.split('-')[2];
+
+            document.getElementById('multi-modal-' + depth_number).remove();
+            document.getElementById('multi-vail-' + depth_number).remove();
+        });
+        modalElement.insertAdjacentElement('beforeend', contentElemnt);
+        modalElement.insertAdjacentElement('beforeend', footerElement);
+        document.body.insertAdjacentElement('beforeend', modalElement);
+        this.zIndex++;
+    };
     return MultiModalControl;
 }());
-MultiModalControl = MultiModalControl;
